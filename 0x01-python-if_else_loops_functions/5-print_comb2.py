d3aca1a466e6f5147d4bc08f9661fe831fad47cd
CalmEnduring <dantelm22@gmail.com>
2022-11-29 15:30:50 -0800

    combo int

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for j in range(10):
-    for i in range(10):
-        print("{}{}, ".format(j, i), end="")
+for i in range(100):
+    print("{:02d}, ".format(i), end="")
 print("\n")
