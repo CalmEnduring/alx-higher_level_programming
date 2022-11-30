2cbcbd3a9a82fee768c895e67ce0e7269156534f
CalmEnduring <dantelm22@gmail.com>
2022-11-30 01:49:18 -0800

    not the same combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,9 @@
 comma = ","
 for i in range(10):
     for j in range(10):
+#if ((i != j and i < j) and i < 9):
         if (i < j):
-            print("{}{} ".format(i, j), end="")
-            if (i != 8 or (i == 8 and j != 9)):
-                print(comma, end="")
+            if (i == 8 and j == 9):
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}, ".format(i, j), end="")
