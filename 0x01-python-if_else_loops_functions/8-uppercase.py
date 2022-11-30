a5fad947ea685cb27b1e1421106436e3db0affb6
CalmEnduring <dantelm22@gmail.com>
2022-11-30 06:57:58 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    nstr = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            nstr += chr(ord(str[i]) - 32)
+            continue
+        nstr += str[i]
+    print(nstr)
