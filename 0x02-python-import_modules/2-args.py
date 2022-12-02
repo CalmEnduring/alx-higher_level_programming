698c32649bb352d59a9055d32fb75b33c7c1acb7
CalmEnduring <dantelm22@gmail.com>
2022-12-01 17:44:57 -0800

    arg me matey

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    sa = sys.argv 
-    larg = len(sa) - 1 # argument count excluding 1st argument
-    # if length of argv > 1
-    if larg > 1:
-        print(larg, "arguments:") # length of argv + arguments
-        for i in range(1, larg + 1): # iterate through each argument - 1st
-            print("{}: {}".format(i, sa[i]))
-    elif larg == 1: # if argv is 1 (no arguments besides file itself)
-        print(larg, "arguments:") # same as 1st if condition
-        for i in range(1, larg + 1): # same as 1st for loop
-            print("{}: {}".format(i, sa[i]))
-    elif larg == 0: # if there are no arguments
-        print(larg, "arguments.")
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        print("{}: {}".format(i, sys.argv[i]))
