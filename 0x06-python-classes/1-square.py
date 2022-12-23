d26805df3327d1007fbb2701f7a0d5c3e6af25d7
CalmEnduring <dantelm22@gmail.com>
2022-12-23 08:59:03 -0800

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class that defines a private attribute"""
+
+
+class Square:
+    """Square Class
+
+    This clas initializes a private attribute
+
+    """
+    def __init__(self, size):
+        self.__size = size
