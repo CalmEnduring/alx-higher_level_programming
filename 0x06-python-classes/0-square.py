b216f77ed1cc8ef26c31a4aa31c47500e2047ddb
CalmEnduring <dantelm22@gmail.com>
2022-12-23 03:54:07 -0800

    empy class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
 """Square module
+
 This is a module with an example class
+
 """
 
+
 class Square:
+
     """Square Class
+
     An empty class that does nothing for now
+
     """
     pass
