df557d715960ab10d274d23dbdec61b2e219b5b1
CalmEnduring <dantelm22@gmail.com>
2022-12-23 10:37:29 -0800

    printing square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Contains a class defintion of a Square"""
+
+
+class Square:
+    """Square Class
+    This class defines the size of the Square
+    with exception handling.
+
+    It also uses the property decorator for the getter and setter methods
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = value
+
+        if value < 0:
+            return ValueError("size must be >= 0")
+
+    def area(self):
+        squareArea = self.__size ** 2
+
+        return (squareArea)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+
+        for i in range(1, self.area() + 1):
+            print('#', end='')
+
+            if i % self.__size == 0 and i > 0:
+                print()
