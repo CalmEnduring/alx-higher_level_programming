9c245ef54b666290db636d6d1541d8b9888bf3fc
CalmEnduring <dantelm22@gmail.com>
2023-01-09 17:17:56 -0800

    checks subclass inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
        the specified class
     """
     if isinstance(obj, a_class) and \
-        issubclass(a_class, obj.__class__) is False:
+       issubclass(a_class, obj.__class__) is False:
         return True
     else:
         return False
