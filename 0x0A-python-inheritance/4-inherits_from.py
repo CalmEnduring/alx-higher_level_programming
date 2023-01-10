18ea97c57b880bd265ea7401aef40a20f11c61c8
CalmEnduring <dantelm22@gmail.com>
2023-01-09 17:16:17 -0800

    checks subclass inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a module to check subclass inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """a function that checks if an object is an instance
+       of a class that inherits directly or indirectly from
+       the specified class
+    """
+    if isinstance(obj, a_class) and \
+        issubclass(a_class, obj.__class__) is False:
+        return True
+    else:
+        return False
