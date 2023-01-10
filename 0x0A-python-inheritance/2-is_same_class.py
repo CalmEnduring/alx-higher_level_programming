54e60cc95e329c9aa3e3aa9afd41e75320e5c4ac
CalmEnduring <dantelm22@gmail.com>
2023-01-09 16:45:11 -0800

    check if with class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a module to check whether an object belongs to a class"""
+
+
+def is_same_class(obj, a_class):
+    """a function that checks whether an object belongs to a class"""
+    if isinstance(object, a_class) is True:
+        return True
+    else:
+        return False
