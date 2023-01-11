53f08ce51f60eabeb2c98dbada6a4c34cdf4e3f3
CalmEnduring <dantelm22@gmail.com>
2023-01-11 01:04:24 -0800

    serialize

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from json import dumps
-
-
-"""a module to convert a string to json"""
+"""a module to return the JSON representation of an object(string)"""
 
 
 def to_json_string(my_obj):
