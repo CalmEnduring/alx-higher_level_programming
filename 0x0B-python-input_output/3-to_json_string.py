8b1af00dbdc97633448c55f958cf02436b0c2109
CalmEnduring <dantelm22@gmail.com>
2023-01-10 16:18:36 -0800

    string to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
+from json import dumps
 """a module to convert a string to json"""
 
 
 def to_json_string(my_obj):
     """a function that converts to json"""
-    return (json.dumps(my_obj))
+    return (dumps(my_obj))
