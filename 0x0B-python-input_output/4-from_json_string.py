55b86c918eac1d72d9dbf9b49727e79dbeefce11
CalmEnduring <dantelm22@gmail.com>
2023-01-10 16:37:27 -0800

    from json to string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from json import loads
+"""a module to convert json to string"""
+
+
+def from_json_string(my_str):
+    """a function that converts json to string"""
+    return (loads(my_str))
